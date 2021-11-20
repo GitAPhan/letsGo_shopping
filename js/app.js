df72323ac5c859a94f826d3fb27caaf8a94905a3
GitAPhan <itsandrewphan@gmail.com>
2021-11-19 17:00:34 -0700

    finished assignment along with the bonus

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 {
-
+    // function to select item
     function item_selection(e) {
+
         console.log('an item has been selected');
+
+        // dives into the selected items childNodes and grabs important info
         let image_src = e.childNodes[1].childNodes[1].src;
         let item_name = e.childNodes[1].childNodes[3].childNodes[1].innerText;
         let item_description = e.childNodes[1].childNodes[3].childNodes[3].innerText;
         let item_price = e.childNodes[1].childNodes[3].childNodes[5].innerText;
-        let new_string = item_name + item_description;
 
+        //    sets the item to display none *pretty useless since a refresh would bring them back*
+        e.style.display = "none";
+
+        // created obect to represent item selected 
         let selected_item = {
             Name: item_name,
             Description: item_description,
@@ -15,41 +21,31 @@
             Image: image_src,
         }
 
+        // grabbed the cookie
         let old_cart_cookie_JSON = Cookies.get('cart');
 
+        // conditional statement to decide if a new cookie can be set
+        // or if there is already an existing item in the cart
         if (old_cart_cookie_JSON == undefined) {
+
+            // converted selected item to JSON and set to cookie
             let new_cookie = [{ selected_item }];
             Cookies.set('cart', JSON.stringify(new_cookie));
         } else {
+
+            // converted cookie back to object
             let old_cart_cookie = JSON.parse(old_cart_cookie_JSON);
             console.log(old_cart_cookie);
+
+            // using the variable length assign next position in array
             let i = old_cart_cookie.length;
-            old_cart_cookie[i] = {selected_item};
-            Cookies.set('cart', JSON.stringify(old_cart_cookie));            
+            old_cart_cookie[i] = { selected_item };
+
+            //  rebaked the cookie
+            Cookies.set('cart', JSON.stringify(old_cart_cookie));
         }
-
-
-        // let cartCookie = {
-        //     [`item_${i}`]: selected_item,
-        // }
-
-
-        // console.log(old_cart_cookie);
-
-        // cartCookie_JSON = JSON.stringify(cartCookie);
-        // console.log(cartCookie_JSON);
-
-        // old_cart_cookie.window['item_' + i] = selected_item;
-        // console.log('this');
-
-        // let new_test_cookie = JSON.parse(old_cart_cookie);
-        // console.log('this');
-
-
     }
-
-    // Cookies.set('cart', 'cart');
-
+    // yays
     console.log('yays');
 
 }
--- js/selection.js
+++ js/selection.js
@@ -1,45 +1,91 @@
+// function to clear cart
+function clear_cart() {
+    Cookies.remove('cart');
 
+    // sorry, I know you said avoid using this but I wasn't able to find a working solution for my use case
+    location.reload(true);
+}
 
-var items_in_cart = JSON.parse(Cookies.get('cart'));
+// function to remove specific item from cart
+function remove_cart_item(item) {
+    // This is to find the position of the object in the array
+    var item_position = Number(item.children[0].innerText);
 
-// for (var i = items_in_cart.length; i > 0; i = i - 1) {
-//     var j = i - 1;
-//     eval('var item_' + j +' = items_in_cart[' + j +'];');
-// }
+    // converted cookie to object
+    var cart_cookies = JSON.parse(Cookies.get('cart'));
 
+    // conditional to remove cart cookie if only 1 item
+    if (cart_cookies.length === 1) {
+        Cookies.remove('cart');
+    } else {
+
+        // remove item at index 'item_position'
+        var cart_cookies_remove = cart_cookies.splice(item_position, 1);
+
+        // the cookie has been rebaked
+        Cookies.set('cart', JSON.stringify(cart_cookies));
+    }
+
+    // sorry, I know you said avoid using this but I wasn't able to find a working solution for my use case
+    location.reload(true);
+    console.log('delete me!');
+}
+
+// parent container
 var cart_container = document.getElementById('cart');
 
-for (i = 0; i < items_in_cart.length; i++) {
-    var item_container = document.createElement('div');
-    item_container.classList.add('food');
 
-    var shell_container = document.createElement('div');
-    shell_container.classList.add('shell');
+// conditional to only run if there are items in the cart
+if (Cookies.get('cart') !== undefined) {
+    // converted cookie to objects
+    var items_in_cart = JSON.parse(Cookies.get('cart'));
 
-    var image_src = document.createElement('img');
-    image_src.src = items_in_cart[i].selected_item.Image;
+    // loop to add items
+    for (i = 0; i < items_in_cart.length; i++) {
 
-    var mask_container = document.createElement('div');
-    mask_container.classList.add('mask');
+        // created main containers
+        var item_container = document.createElement('div');
+        item_container.classList.add('food');
+        item_container.setAttribute('onclick', 'remove_cart_item(this)');
+        var shell_container = document.createElement('div');
+        shell_container.classList.add('shell');
 
-    var item_name = document.createElement('h3');
-    item_name.innerText = items_in_cart[i].selected_item.Name;
+        // creates invisible ID tag
+        var secret_tag = document.createElement('p');
+        secret_tag.innerText = i;
+        secret_tag.style.display = 'none';
+        item_container.appendChild(secret_tag);
+        // creates image
+        var image_src = document.createElement('img');
+        image_src.src = items_in_cart[i].selected_item.Image;
+        // creates color mask
+        var mask_container = document.createElement('div');
+        mask_container.classList.add('mask');
+        // creates item name h3 tag
+        var item_name = document.createElement('h3');
+        item_name.innerText = items_in_cart[i].selected_item.Name;
+        // creates ingredients/description p tag
+        var item_description = document.createElement('p');
+        item_description.innerText = items_in_cart[i].selected_item.Description;
+        item_description.classList.add('ingredients');
+        // creates price h2 tag
+        var item_price = document.createElement('h2');
+        item_price.innerText = items_in_cart[i].selected_item.Price;
 
-    var item_description = document.createElement('p');
-    item_description.innerText = items_in_cart[i].selected_item.Description;
-    item_description.classList.add('ingredients');
+        // let the appending begin!
+        mask_container.appendChild(item_name);
+        mask_container.appendChild(item_description);
+        mask_container.appendChild(item_price);
+        shell_container.appendChild(image_src);
+        shell_container.appendChild(mask_container);
+        item_container.appendChild(shell_container);
+        cart_container.appendChild(item_container);
 
-    var item_price = document.createElement('h2');
-    item_price.innerText = items_in_cart[i].selected_item.Price;
-
-
-    mask_container.appendChild(item_name);
-    mask_container.appendChild(item_description);
-    mask_container.appendChild(item_price);
-    shell_container.appendChild(image_src);
-    shell_container.appendChild(mask_container);
-    item_container.appendChild(shell_container);
-    cart_container.appendChild(item_container);
-
+    }
+} else {
+    // message
+    cart_container.innerText = "you currently don't have any items selected!";
+    cart_container.style.textAlign = 'center';
+    cart_container.style.fontSize = '2em';
 }
-console.log(this);
+
